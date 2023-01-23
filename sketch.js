d612e79139c7191e5c3cd4dde14df138503f4b8e
Jéssica Venâncio <120992355+jessicavenancio@users.noreply.github.com>
2023-01-23 19:27:02 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,27 @@
+//criar variáveis
+
+
+
+
+//função que carrega imagens
+{
+    //carregar imagens
+}
+
+
+//função que inicializa o arquivo
+{
+    //cria tamanho do fundo da tela
+    //criar sprites
+    //carregar imagem
+    //colocar tamanho
+}
+
+
+//função que executa o programa até que seja parado
+{
+    //colocar cor de fundo
+    //condição para pular
+    //exibir texto de instrução
+    //comando que desenha sprites
+}
